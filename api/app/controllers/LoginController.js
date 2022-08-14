df567de90cf48a8cbd6a52f387d8a8a308d6ab4f
Ana Sofia <sofiamelod@gmail.com>
2022-08-14 16:16:21 -0300

    organizando a função de login (index)

--- api/app/controllers/LoginController.js
+++ api/app/controllers/LoginController.js
@@ -1,42 +1,42 @@
-require('dotenv').config()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const database = require('../models/')
-
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const database = require("../models/");
 class LoginController {
+  async index(req, res) {
+    const { user, senha } = req.body;
+    const userExist = await database.User.findOne({ where: { user } });
+    try {
+      const senhaMatch = await bcrypt.compare(senha, userExist.senha);
+      if (!userExist) {
+        return res.status(400).send("Usuário inválido");
+      } else if (!senhaMatch) {
+        return res.status(400).send("Senha inválida");
+      } else {
+        const userObject = {
+          id: userExist.id,
+          user: userExist.user,
+          email: userExist.email,
+          nome: userExist.nome,
+          profileDesc: userExist.profileDesc,
+          admin: userExist.admin,
+        };
 
-    async index(req, res){
-        const { user, senha } = req.body
-        
+        const accessToken = jwt.sign(
+          userObject,
+          process.env.ACCESS_TOKEN_SECRET
+        );
 
-        const userExist = await database.User.findOne({ where: {user: user}})
-        try {
-            const senhaMatch = await bcrypt.compare(senha, userExist.senha)
-            if (!userExist){
-                return res.status(400).send('USuário inválido')
-            } else if (!senhaMatch ) {
-                return res.status(400).send('Senha inválida')
-            } else {
-                const accessToken = jwt.sign( { id: userExist.id, user: userExist.user, nome: userExist.nome, email: userExist.email, admin: userExist.admin, profileDesc: userExist.profileDesc }, process.env.ACCESS_TOKEN_SECRET)
-                res.header('Authorization', accessToken)
-                return res.status(200).json({
-                    user: {
-                        id: userExist.id,
-                        user: userExist.user,
-                        email: userExist.email,
-                        nome: userExist.nome,
-                        profileDesc: userExist.profileDesc,
-                        admin: userExist.admin,
-                        accessToken: accessToken
-                    }
-                })
-            }
+        res.header("Authorization", accessToken);
 
-        } catch (error) {
-            res.status(500).send(error.message)
-        }
-
+        return res.status(200).json({
+          user: { ...userObject, accessToken },
+        });
+      }
+    } catch (e) {
+      res.status(500).send(e.message);
     }
+  }
 }
 
 module.exports = new LoginController();
